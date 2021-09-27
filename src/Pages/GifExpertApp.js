fc398219fe69cea6cddabdabcc461c12cf236e9a
Jhonatan Quirama Pino <50423635+kira4489@users.noreply.github.com>
2021-09-27 17:59:53 -0500

    fix folders

--- src/Pages/GifExpertApp.js
+++ src/Pages/GifExpertApp.js
@@ -1,6 +1,6 @@
 import React, { useState } from "react";
-import { AddCategory } from "./Components/AddCategory";
-import { GifGrid } from "./Components/GifGrid";
+import { AddCategory } from "../Components/AddCategory";
+import { GifGrid } from "../Components/GifGrid";
 
 export const GifExpertApp = () => {
   //const categories = ["one punch","samurai x","dragon ball"]
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import { GifExpertApp } from "./GifExpertApp.js"
+import { GifExpertApp } from "./Pages/GifExpertApp"
 
 ReactDOM.render(
     <GifExpertApp/>,
